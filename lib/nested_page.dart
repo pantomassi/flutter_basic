50227155676d786fe2098f794df33d8063025816
Pantomassi <tmusial10@gmail.com>
2023-02-07 03:40:48 +0100

    finish NestedPage

--- lib/nested_page.dart
+++ lib/nested_page.dart
@@ -8,6 +8,8 @@ class NestedPage extends StatefulWidget {
 }
 
 class _NestedPageState extends State<NestedPage> {
+  bool isSwitched = false;
+  bool? isChecked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,57 +22,92 @@ class _NestedPageState extends State<NestedPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Action from IconButton');
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
       ),
-      body: Column(
-        children: [
-          Image.asset(
-            'images/doggo_sm.jpg',
-          ),
-          const SizedBox(
-            height: 2,
-          ),
-          const Divider(color: Colors.black),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                'A Text widget!',
-                style: TextStyle(
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'images/doggo_sm.jpg',
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            const Divider(color: Colors.black),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  'A Text widget!',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              debugPrint('Inside body/elevated button');
-            },
-            child: const Text('Hiya, Elevated button here'),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint('Inside body/outlined button');
-            },
-            child: const Text('Hiya, Outlined button here'),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint('Inside body/Text button');
-            },
-            child: const Text('Hiya, Text button here'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Icon(Icons.local_fire_department, color: Colors.amber),
-              Text('Row widget between icons'),
-              Icon(Icons.local_fire_department, color: Colors.amber),
-            ],
-          ),
-        ],
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitched ? Colors.blue : Colors.grey,
+              ),
+              onPressed: () {
+                debugPrint('Inside body/elevated button');
+              },
+              child: const Text('Hiya, Elevated button here'),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint('Inside body/outlined button');
+              },
+              child: const Text('Hiya, Outlined button here'),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint('Inside body/Text button');
+              },
+              child: const Text('Hiya, Text button here'),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint('Tapped the row');
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(Icons.local_fire_department, color: Colors.blue),
+                  Text('Row widget between icons'),
+                  Icon(Icons.local_fire_department, color: Colors.blue),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitched,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitched = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isChecked,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isChecked = newBool;
+                  });
+                }),
+            Image.network(
+                'https://img.freepik.com/free-photo/front-view-adorable-dog-looking-up_23-2148366944.jpg')
+          ],
+        ),
       ),
     );
   }
