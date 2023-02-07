70e0600b0804045ea3e49a19b02b3db2c5ac0fe5
Pantomassi <tmusial10@gmail.com>
2023-02-07 02:37:10 +0100

    Add row to NestedPage

--- lib/nested_page.dart
+++ lib/nested_page.dart
@@ -23,19 +23,52 @@ class _NestedPageState extends State<NestedPage> {
       ),
       body: Column(
         children: [
-          Image.asset('images/doggo.jpg'),
+          Image.asset(
+            'images/doggo_sm.jpg',
+          ),
           const SizedBox(
             height: 2,
           ),
           const Divider(color: Colors.black),
           Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             color: Colors.blueGrey,
-            child: const Text(
-              'A Text widget!',
-              style: TextStyle(
-                color: Colors.white,
+            width: double.infinity,
+            child: const Center(
+              child: Text(
+                'A Text widget!',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
               ),
             ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint('Inside body/elevated button');
+            },
+            child: const Text('Hiya, Elevated button here'),
+          ),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint('Inside body/outlined button');
+            },
+            child: const Text('Hiya, Outlined button here'),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint('Inside body/Text button');
+            },
+            child: const Text('Hiya, Text button here'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: const [
+              Icon(Icons.local_fire_department, color: Colors.amber),
+              Text('Row widget between icons'),
+              Icon(Icons.local_fire_department, color: Colors.amber),
+            ],
           ),
         ],
       ),
