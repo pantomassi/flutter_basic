cc6a68358187768fd17f29e84d0e2ac05d2d1135
Pantomassi <tmusial10@gmail.com>
2023-02-07 04:16:06 +0100

    build ProfilePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/home_page.dart';
+import 'package:flutter_basic/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,13 +28,17 @@ class RootPage extends StatefulWidget {
 
 class _RootPageState extends State<RootPage> {
   int currentPage = 0;
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('FlutteryApp'),
       ),
-      body: const HomePage(),
+      body: pages[currentPage],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint('Sent from FloatingAction button!!!');
